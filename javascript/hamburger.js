8d107884085cde1345fba809b1c088a8e4258369
Bikelund <nicklas_barklund@hotmail.com>
2019-02-11 12:48:37 +0100

    Hamburger menu fix

--- javascript/hamburger.js
+++ javascript/hamburger.js
@@ -8,18 +8,20 @@
 /* Close the dropdown if the user clicks outside of it */
 window.onclick = function(event) {
     if (!event.target.matches('.dropbtn')) {
-        var dropdowns = document.getElementsByClassName("dropdown-content");
-        var i;
-        for (i = 0; i < dropdowns.length; i++) {
-            var openDropdown = dropdowns[i];
+        let dropdowns = document.getElementsByClassName("dropdown-content");
+        for (let i = 0; i < dropdowns.length; i++) {
+            let openDropdown = dropdowns[i];
             if (openDropdown.classList.contains('show')) {
             openDropdown.classList.remove('show');
+            document.getElementsByTagName("body")[0].classList.remove("no-scroll");
             }
         }
     }
 }
 
-/* Hide and show dropdown content */ 
+/* Hide and show dropdown content */
+/* Makes background content non scrollable */ 
 function myFunction() {
     document.getElementById("myDropdown").classList.toggle("show");
+    document.getElementsByTagName("body")[0].classList.toggle("no-scroll");
 }
